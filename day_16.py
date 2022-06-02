9a82cd54db8997c1b04a7cdbdaba15d5919a5510
Royal dahal <58435678+royal-dit@users.noreply.github.com>
2022-06-02 21:52:32 +0545

    Add files via upload

---
+++ day_16.py
@@ -0,0 +1,14 @@
+# oops in pyhton\
+
+# pokemon name  table using pretty table package
+
+
+# from prettytable import PrettyTable
+#
+# table = PrettyTable()
+# table.add_column("Pokemon Name",["pikachu","squirtle","Charmander"])
+# table.add_column("Type",["Electric","Water","Fire"])
+# table.align= "l"
+# print(table)
+
+
